5ab054adf770b9cf0d32a782f581e2b1af3e5b5d
Tzsapphire <cheezy.ep@gmail.com>
2026-04-12 06:20:23 +0200

    created connection to postgres source engine

--- connections.py
+++ connections.py
@@ -19,4 +19,29 @@ from dotenv import load_dotenv
 load_dotenv('conn_setup.env')
 
 
+# creating source engine connection function
+
+def pg_engine():
+    pg_sa_url = sa_URL.create(
+        drivername="postgresql+psycopg2",
+        username= os.getenv("postgres_user"),
+        password= os.getenv("postgres_password"),
+        host= os.getenv("postgres_host"),
+        port=os.getenv("postgres_port"),
+        database= os.getenv("postgres_database")
+    )
+    print('creating engine connection to source... \n')
+    try:
+        src_engine = create_engine(pg_sa_url)
+        with src_engine.connect() as pg_conn:
+            query=text('select CURRENT_DATABASE();')
+            print('testing database connection...')
+            pg_database = pg_conn.execute(query).scalar()
+            print("connected to database: ", pg_database, "\n")
+        return src_engine
+    except Exception as e:
+        print('error connecting to database, details: ', e)
+
+
+pg_engine()
 
